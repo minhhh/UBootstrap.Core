449285542b37a3ef5aa6da95d4529a93d7ee96bd
Ha.Minh <minhhh@minhhuyha.info>
2017-09-29 14:37:27 +0700

    v0.0.17

--- Assets/UBootstrap.Core/Plugins/Helpers/EnumHelper.cs
+++ Assets/UBootstrap.Core/Plugins/Helpers/EnumHelper.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace UBootstrap
 {
     static public partial class EnumHelper
     {
+        static Dictionary <Enum, string> _enumToStringDict = new Dictionary <Enum, string> ();
+        static Dictionary <Enum, string> _enumToStringUpperDict = new Dictionary <Enum, string> ();
+        static Dictionary <Enum, string> _enumToStringLowerDict = new Dictionary <Enum, string> ();
+
         public static int EnumToInt (Enum e)
         {
             return (int)((object)e);
@@ -14,5 +19,31 @@ namespace UBootstrap
             return EnumToInt (a) == EnumToInt (b);
         }
 
+        public static string EnumToString (Enum e)
+        {
+            if (!_enumToStringDict.ContainsKey (e)) {
+                _enumToStringDict [e] = e.ToString ();
+            }
+
+            return _enumToStringDict [e];
+        }
+
+        public static string EnumToStringUpper (Enum e)
+        {
+            if (!_enumToStringUpperDict.ContainsKey (e)) {
+                _enumToStringUpperDict [e] = e.ToString ().ToUpper ();
+            }
+
+            return _enumToStringUpperDict [e];
+        }
+
+        public static string EnumToStringLower (Enum e)
+        {
+            if (!_enumToStringLowerDict.ContainsKey (e)) {
+                _enumToStringLowerDict [e] = e.ToString ().ToLower ();
+            }
+
+            return _enumToStringLowerDict [e];
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -22,6 +22,10 @@ All the classes are documented [here](https://github.com/minhhh/UBootstrap.Core/
 
 ## Changelog
 
+**0.0.17**
+
+* Update EnumHelper
+
 **0.0.16**
 
 * Bump version
